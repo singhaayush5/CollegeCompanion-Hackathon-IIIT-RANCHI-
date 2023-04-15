92227ac49f9cc6dc68f2543d64bf04e42520f74d
Aayush Singh <aayushs1432@gmail.com>
2023-04-15 23:32:57 +0530

    frontend login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,32 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Job from "./Component/JobPortal/JobPortal";
+import AttendancePage from "./Component/attendance/attendancepage";
+import Login from "./Component/authentication/login";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Signup from "./Component/authentication/register";
 
 function App() {
-  return (
-    <div className="App">
-      <Job />
-      Hello
-    </div>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/">
+        <Route path="user">
+          <Route path="login" element={<Login/>}></Route>
+          <Route path="register" element={<Signup/>}></Route>
+          <Route path=":id">
+            <Route path="attendance" element={<AttendancePage/>}></Route>
+          </Route>
+        </Route>
+      </Route>
+    )
   );
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
