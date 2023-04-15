6df1ae5855b98ae1e71661d2e4b08bafa1d7ef88
Sumit <sumitagarwal2002@gmail.com>
2023-04-15 17:10:43 +0530

    initial commit

---
+++ server/model/admin.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+//const jwt = require("jsonwebtoken");
+
+const adminSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+ 
+});
+
+
+const adminuser = mongoose.model("ADMINUSER", adminSchema);
+
+module.exports = adminuser;
